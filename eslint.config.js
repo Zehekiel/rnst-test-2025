f5d3b5b24ce1d6971401567e0c22b8db0fa541f6
Quentin Guérin <qguerin@cegid.com>
2025-04-26 17:19:00 +0200

    style: :art: install prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
   {
-		rules: {
-			"no-unused-vars": "warn",
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
       "no-console": "warn",
       "no-duplicate-imports": "error",
       "no-multi-spaces": "error",
@@ -22,6 +23,10 @@ export default defineConfig([
       "indent": ["error", 4],
       "eqeqeq": 'error',
       "no-debugger": "error",
-		},
-	},
-]);
+      'quotes': 'off',
+      '@typescript-eslint/quotes': ['error', 'double'],
+      "semi": ["error", "always"],
+    },
+  },
+  eslintConfigPrettier
+])
